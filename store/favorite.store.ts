1de9d0c16452ae3761a3cd399603d1a321412481
Amiw <wms36155@gmail.com>
2026-05-26 13:02:23 +0500

    problems

--- store/favorite.store.ts
+++ store/favorite.store.ts
@@ -1,58 +1,36 @@
 import { create } from "zustand";
-
 import { Equipment } from "@/types/equipment.types";
 
 type FavoriteStore = {
   favorites: Equipment[];
-
-  toggleFavorite: (
-    equipment: Equipment
-  ) => void;
-
-  isFavorite: (
-    id: number
-  ) => boolean;
+  toggleFavorite: (equipment: Equipment) => void;
+  isFavorite: (id: string) => boolean;
 };
 
-export const useFavoriteStore =
-  create<FavoriteStore>(
-    (set, get) => ({
-      favorites: [],
+export const useFavoriteStore = create<FavoriteStore>((set, get) => ({
+  favorites: [],
 
-      toggleFavorite: (
-        equipment
-      ) => {
-        const exists =
-          get().favorites.find(
-            (item) =>
-              item.id ===
-              equipment.id
-          );
+  toggleFavorite: (equipment) => {
+    const exists = get().favorites.find(
+      (item) => item.id === equipment.id
+    );
 
-        if (exists) {
-          set({
-            favorites:
-              get().favorites.filter(
-                (item) =>
-                  item.id !==
-                  equipment.id
-              ),
-          });
-        } else {
-          set({
-            favorites: [
-              ...get().favorites,
-              equipment,
-            ],
-          });
-        }
-      },
+    if (exists) {
+      set({
+        favorites: get().favorites.filter(
+          (item) => item.id !== equipment.id
+        ),
+      });
+    } else {
+      set({
+        favorites: [...get().favorites, equipment],
+      });
+    }
+  },
 
-      isFavorite: (id) => {
-        return !!get().favorites.find(
-          (item) =>
-            item.id === id
-        );
-      },
-    })
-  );
+  isFavorite: (id) => {
+    return !!get().favorites.find(
+      (item) => item.id === id
+    );
+  },
+}));
